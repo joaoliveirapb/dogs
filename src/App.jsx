b686cbefddb626963e7e9021592c2c944dfd984c
joaoliveirapb <joaoliveira.batista1@gmail.com>
2023-07-11 18:55:52 -0300

    feat: definition of routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import { Footer } from './components/Footer/Footer'
+import { Header } from './components/Header/Header'
+import { Home } from './pages/Home'
+import { Login } from './pages/Login'
 
 export default function App() {
-  return <div>Hello World!</div>
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  )
 }
