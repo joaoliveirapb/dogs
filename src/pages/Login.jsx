acb561fa5f4b8e4745e4b21af44cb402599df81d
joaoliveirapb <joaoliveira.batista1@gmail.com>
2023-07-11 20:07:00 -0300

    feat: created the login page routes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,3 +1,18 @@
+import { Route, Routes } from 'react-router-dom'
+import { LoginCreate } from '../components/Login/LoginCreate'
+import { LoginForm } from '../components/Login/LoginForm'
+import { LoginPasswordLost } from '../components/Login/LoginPasswordLost'
+import { LoginPasswordReset } from '../components/Login/LoginPasswordReset'
+
 export function Login() {
-  return <div>Login</div>
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="criar" element={<LoginCreate />} />
+        <Route path="perdeu" element={<LoginPasswordLost />} />
+        <Route path="resetar" element={<LoginPasswordReset />} />
+      </Routes>
+    </div>
+  )
 }
