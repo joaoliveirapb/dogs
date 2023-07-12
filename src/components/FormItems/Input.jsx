27bcf954680ba0b4ac844f6f2d7f8e0199a69984
joaoliveirapb <joaoliveira.batista1@gmail.com>
2023-07-11 21:53:10 -0300

    feat: add the rest of the properties to the input

--- src/components/FormItems/Input.jsx
+++ src/components/FormItems/Input.jsx
@@ -1,13 +1,21 @@
 import styles from './Input.module.css'
 
-export function Input({ label, type, name }) {
+export function Input({ label, type, name, value, onChange, error, onBlur }) {
   return (
     <div className={styles.wrapper}>
       <label htmlFor={name} className={styles.label}>
         {label}
       </label>
-      <input type={type} id={name} name={name} className={styles.input} />
-      <p className={styles.error}>Erro</p>
+      <input
+        type={type}
+        id={name}
+        name={name}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+        className={styles.input}
+      />
+      {error && <p className={styles.error}>{error}</p>}
     </div>
   )
 }
