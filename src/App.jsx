0564ff506090508ca64450a6ca8e8003c5b96bf3
joaoliveirapb <joaoliveira.batista1@gmail.com>
2023-07-11 23:37:08 -0300

    feat: created to log in and get user information

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import { UserStorage } from './UserContext'
 import { Footer } from './components/Footer/Footer'
 import { Header } from './components/Header/Header'
 import { Home } from './pages/Home'
@@ -9,12 +10,14 @@ export default function App() {
   return (
     <div>
       <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login/*" element={<Login />} />
-        </Routes>
-        <Footer />
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login/*" element={<Login />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
       </BrowserRouter>
     </div>
   )
