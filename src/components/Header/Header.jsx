19f321fc22bd7e6a720ab5b2cb75275be032b1f0
joaoliveirapb <joaoliveira.batista1@gmail.com>
2023-07-12 19:11:38 -0300

    refactor: logout button removed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ import { ReactComponent as Dogs } from '../../assets/dogs.svg'
 import styles from './Header.module.css'
 
 export function Header() {
-  const { data, userLogout } = useContext(UserContext)
+  const { data } = useContext(UserContext)
 
   return (
     <header className={styles.header}>
@@ -18,7 +18,6 @@ export function Header() {
           <Link className={styles.login} to="/conta">
             {data.nome}
             <User size={20} />
-            <button onClick={userLogout}>Sair</button>
           </Link>
         ) : (
           <Link className={styles.login} to="/login">
