2565af5ea915f6769170faa69bcde8c7deca0efe
joaoliveirapb <joaoliveira.batista1@gmail.com>
2023-07-11 23:39:26 -0300

    feat: displaying the name of the logged in user

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,19 +1,30 @@
 import { User } from 'lucide-react'
+import { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { UserContext } from '../../UserContext'
 import { ReactComponent as Dogs } from '../../assets/dogs.svg'
 import styles from './Header.module.css'
 
 export function Header() {
+  const { data } = useContext(UserContext)
+
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} container`}>
         <Link className={styles.logo} to="/" aria-label="Dogs - Home">
           <Dogs />
         </Link>
-        <Link className={styles.login} to="/login">
-          Login / Criar
-          <User size={20} />
-        </Link>
+        {data ? (
+          <Link className={styles.login} to="/conta">
+            {data.nome}
+            <User size={20} />
+          </Link>
+        ) : (
+          <Link className={styles.login} to="/login">
+            Login / Criar
+            <User size={20} />
+          </Link>
+        )}
       </nav>
     </header>
   )
