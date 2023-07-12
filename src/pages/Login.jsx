a2dfc19717bdbcf60f97eae91eaa01cb65b5698d
joaoliveirapb <joaoliveira.batista1@gmail.com>
2023-07-12 12:41:14 -0300

    feat: created styles for the login route

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,18 +1,27 @@
-import { Route, Routes } from 'react-router-dom'
+import { useContext } from 'react'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { UserContext } from '../UserContext'
 import { LoginCreate } from '../components/Login/LoginCreate'
 import { LoginForm } from '../components/Login/LoginForm'
 import { LoginPasswordLost } from '../components/Login/LoginPasswordLost'
 import { LoginPasswordReset } from '../components/Login/LoginPasswordReset'
+import styles from './Login.module.css'
 
 export function Login() {
+  const { login } = useContext(UserContext)
+
+  if (login === true) return <Navigate to="/conta" />
+
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="criar" element={<LoginCreate />} />
-        <Route path="perdeu" element={<LoginPasswordLost />} />
-        <Route path="resetar" element={<LoginPasswordReset />} />
-      </Routes>
-    </div>
+    <section className={styles.login}>
+      <div className={styles.forms}>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="criar" element={<LoginCreate />} />
+          <Route path="perdeu" element={<LoginPasswordLost />} />
+          <Route path="resetar" element={<LoginPasswordReset />} />
+        </Routes>
+      </div>
+    </section>
   )
 }
