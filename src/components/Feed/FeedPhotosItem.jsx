54e6969d376dea34603a615885bf5d8bc91d6639
joaoliveirapb <joaoliveira.batista1@gmail.com>
2023-07-13 01:40:37 -0300

    feat: each img that will be displayed in the feed

---
+++ src/components/Feed/FeedPhotosItem.jsx
@@ -0,0 +1,18 @@
+import { Eye } from 'lucide-react'
+import styles from './FeedPhotosItem.module.css'
+
+export function FeedPhotosItem({ photo, setModalPhoto }) {
+  function handleClick() {
+    setModalPhoto(photo)
+  }
+
+  return (
+    <li className={styles.photo} onClick={handleClick}>
+      <img src={photo.src} alt={photo.title} />
+      <span className={styles.visualizacao}>
+        <Eye size={20} />
+        {photo.acessos}
+      </span>
+    </li>
+  )
+}
