54109a88c9d505da95a2e1fbbe956d95c800d162
joaoliveirapb <joaoliveira.batista1@gmail.com>
2023-07-15 00:05:09 -0300

    feat: input field for posting comments

---
+++ src/components/Photo/PhotoCommentsForm.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react'
+import { COMMENT_POST } from '../../api'
+import { ReactComponent as Enviar } from '../../assets/enviar.svg'
+import { useFetch } from '../../hooks/useFetch'
+import { Error } from '../Helper/Error'
+import styles from './PhotoCommentsForm.module.css'
+
+export function PhotoCommentsForm({ id, setComments }) {
+  const [comment, setComment] = useState('')
+  const { request, error } = useFetch()
+
+  async function handleSubmit(e) {
+    e.preventDefault()
+    const { url, options } = COMMENT_POST(id, { comment })
+    const { response, json } = await request(url, options)
+    if (response.ok) {
+      setComment('')
+      setComments((comments) => [...comments, json])
+    }
+  }
+
+  return (
+    <form onSubmit={handleSubmit} className={styles.form}>
+      <textarea
+        id="comment"
+        name="comment"
+        placeholder="Comente..."
+        value={comment}
+        onChange={(e) => setComment(e.target.value)}
+        className={styles.textarea}
+      />
+      <button className={styles.button}>
+        <Enviar />
+      </button>
+      <Error error={error} />
+    </form>
+  )
+}
