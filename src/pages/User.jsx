d8a1f71285c043143b1eafc90dfa3124f004d202
joaoliveirapb <joaoliveira.batista1@gmail.com>
2023-07-16 23:33:55 -0300

    feat: display only the photos you have posted

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,15 +1,19 @@
+import { useContext } from 'react'
 import { Route, Routes } from 'react-router-dom'
+import { UserContext } from '../UserContext'
 import { Feed } from '../components/Feed/Feed'
 import { UserHeader } from '../components/User/UserHeader'
 import { UserPhotoPost } from '../components/User/UserPhotoPost'
 import { UserStats } from '../components/User/UserStats'
 
 export function User() {
+  const { data } = useContext(UserContext)
+
   return (
     <section className="container">
       <UserHeader />
       <Routes>
-        <Route path="/" element={<Feed />} />
+        <Route path="/" element={<Feed user={data.id} />} />
         <Route path="/postar" element={<UserPhotoPost />} />
         <Route path="/estatisticas" element={<UserStats />} />
       </Routes>
